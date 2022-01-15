9abd7231095d0f8f0be4cdf23028fb55d4570eef
Rajath <rajath.kumar@novigo.com>
2022-01-15 22:35:59 +0530

    fix linter errors in index.js

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-console.log("Hello");
+console.log('Hello');
 
 function test() {
   const result = 1 + 1;
-  console.log("Test code is called ", result);
+  console.log('Test code is called ', result);
 }
 
 console.log(test());
